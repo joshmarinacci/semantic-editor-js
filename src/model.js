a4c04328343737b3d71efa48e7712399d7cee011
Josh Marinacci <joshua@marinacci.org>
2015-07-09 10:23:25 -0700

    added iterator w/ unit test

--- src/model.js
+++ src/model.js
@@ -2,6 +2,9 @@
  * Created by josh on 7/8/15.
  */
 
+exports.TEXT = 'text';
+exports.SPAN = 'span';
+
 var _id_count = 0;
 exports.genId = function() {
     _id_count++;
@@ -14,22 +17,23 @@ function DNode(type,text) {
     this.type = type;
     this.id = exports.genId();
     this.style = DEFAULT_BLOCK_STYLE;
+    this.parent = null;
 
     if(type == 'block' || type == 'span' || type == 'root') {
         this.content = [];
         this.append = function(node) {
             this.content.push(node);
-        }
+            node.parent = this;
+        };
         this.child = function(index) {
             return this.content[index];
-        }
+        };
     }
-    if(type == 'text') {
+    if(type == exports.TEXT) {
         this.text = text;
     }
 }
 
-var TEXT = 'text';
 
 function countChars(par) {
     var total = 0;
@@ -38,7 +42,7 @@ function countChars(par) {
             total += countChars(n);
         })
     }
-    if(par.type == TEXT) {
+    if(par.type == exports.TEXT) {
         return par.text.length;
     }
     return total;
@@ -46,8 +50,79 @@ function countChars(par) {
 
 function flattenChars(par) {
     if(par.content) return par.content.map(flattenChars).join("");
-    if(par.type == TEXT) return par.text;
+    if(par.type == exports.TEXT) return par.text;
     throw new Error("SHOULDNT BE HERE");
+}
+
+//this is a post-order traversal. handle children before the parent
+function DNodeIterator(current) {
+    this.current = current;
+    this.cameup = false;
+    console.log("making a dnode iterator. starting at",this.current.id);
+
+    function getIndex(node) {
+        var parent = node.parent;
+        var n = parent.content.indexOf(node);
+        return n;
+    }
+    this.getNextSibling = function() {
+        if(this.current.parent == null) return null;
+        var parent = this.current.parent;
+        var n = parent.content.indexOf(this.current);
+        if(n < parent.content.length-1) {
+            return parent.content[n+1];
+        }
+        return null;
+    };
+
+    this.getChildren = function() {
+        if(this.current.content) return this.current.content;
+        return [];
+    };
+
+
+
+    this.hasNext = function() {
+        if(this.cameup === false) {
+            //look at kids
+            var kids = this.getChildren();
+            if (kids.length > 0) return true;
+        }
+        //look at sibling
+        var sib = this.getNextSibling();
+        if(sib != null) return true;
+
+        //look at parent
+        if(this.current.parent !== null) {
+            return true;
+        }
+        return false;
+    };
+
+    this.next = function() {
+
+        //look at kids
+        if(this.cameup == false) {
+            var kids = this.getChildren();
+            if (kids.length > 0) {
+                this.current = kids[0];
+                return this.current;
+            }
+        }
+        this.cameup = false;
+        //look at sibling
+        var sib = this.getNextSibling();
+        if(sib != null) {
+            this.current = sib;
+            return this.current;
+        }
+
+        //go up to parent
+        this.current = this.current.parent;
+        this.cameup = true;
+        return this.current;
+    }
+
 }
 
 function DModel() {
@@ -57,8 +132,11 @@ function DModel() {
         return new DNode('block');
     };
     this.makeText = function(text) {
-        return new DNode(TEXT,text);
+        return new DNode(exports.TEXT,text);
     };
+    this.makeSpan = function() {
+        return new DNode(exports.SPAN);
+    }
     this.append = function(node) {
         root.append(node);
     };
@@ -68,7 +146,7 @@ function DModel() {
     };
 
     this.insertText = function(node, offset, text) {
-        if(node.type == TEXT) {
+        if(node.type == exports.TEXT) {
             node.text = node.text.substring(0,offset) + text + node.text.substring(offset);
         }
     };
@@ -80,7 +158,7 @@ function DModel() {
             return;
         }
 
-        if(startNode.type !== TEXT) throw new Error("can't delete from non text yet");
+        if(startNode.type !== exports.TEXT) throw new Error("can't delete from non text yet");
         if(startOffset > endOffset) throw new Error("start offset can't be greater than end offset");
         startNode.text = startNode.text.substring(0,startOffset)
             + startNode.text.substring(endOffset);
@@ -91,6 +169,11 @@ function DModel() {
     };
 
     this.getRoot = function() { return root};
+
+
+    this.getIterator = function(node) {
+        return new DNodeIterator(node);
+    }
 }
 
 exports.makeModel = function() {
--- tests/test_model.js
+++ tests/test_model.js
@@ -73,4 +73,86 @@ test('delete text, two text nodes',function(t) {
     t.end();
 });
 
+function randomTree() {
+    var model = doc.makeModel();
+    var root = model.getRoot();
+    var familySize = Math.floor(Math.random()*4);
+    for(var i=0; i<familySize; i++) {
+        var block = model.makeBlock();
+        root.append(block);
+        randomTreeHelper(model,block,1);
+    }
+    return model;
+}
 
+function randomTreeHelper(model,root,depth) {
+    var familySize = Math.floor(Math.random()*4);
+    for(var i=0; i<familySize; i++) {
+        if(i%2 == 0) {
+            root.append(model.makeText("some text"));
+        } else {
+            var span = model.makeSpan();
+            root.append(span);
+            span.append(model.makeText("some text"));
+        }
+    }
+}
+
+test('iterator',function(t) {
+    var model = doc.makeModel();
+    var block1 = model.makeBlock();
+    var text1 = model.makeText("foo");
+    block1.append(text1);
+    var text2 = model.makeText("bar");
+    block1.append(text2);
+    model.append(block1);
+
+    var block2 = model.makeBlock();
+    model.append(block2);
+
+    dumpTree(model.getRoot());
+    var it = model.getIterator(model.getRoot().child(0));
+    var total = 0;
+    while(it.hasNext()) {
+        var node = it.next();
+        console.log("next is ",node.id);
+        if(node.type == doc.TEXT) {
+            total+= node.text.length;
+        }
+    }
+    t.equal(model.countCharacters(),total);
+    t.end();
+});
+
+function dumpTree(root,indent) {
+    if(!indent) indent = "";
+    if(root.type == doc.TEXT) {
+        console.log(indent+root.id+" "+root.type+" '"+root.text+"'");
+    }else {
+        console.log(indent + root.id+" "+root.type);
+    }
+    if(root.content) {
+        root.content.forEach(function(node) {
+            dumpTree(node,indent + "  ");
+        })
+    }
+}
+
+/*
+test("delete text across two blocks w/ text inbetween", function(t) {
+    var model = makeSimpleTextBlock("abcdefg");
+    var block1 = model.getRoot().child(0);
+    var text1  = block1.child(0);
+    var text1a = model.makeText("hijklmnop");
+    block1.append(text1a);
+
+    var block2 = model.makeBlock();
+    var text2 = model.makeText("qrstuvwxyz");
+    block2.append(text2);
+    model.append(block2);
+
+    model.deleteText(text1,1,text2,9);
+    t.equal(model.toPlainText(),'az');
+    t.end();
+});
+    */
